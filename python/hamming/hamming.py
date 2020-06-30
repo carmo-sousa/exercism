7ef56ee0a32cde349aebc51d7e12db32577c195c
Carmo-sousa <carmosousa20@outlook.com>
2020-06-30 09:15:23 -0400

    Exercício hamming resolvido na linguagem python 3.x

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,19 @@
 def distance(strand_a, strand_b):
-    pass
+    length_a = len(strand_a)
+    length_b = len(strand_b)
+
+    distance = 0
+
+    if length_a != length_b:
+        raise ValueError('Tamanhos diferentes')
+
+    else:
+        count = 0
+
+        for item in strand_a:
+            if strand_b[count] != item:
+                distance += 1
+            
+            count += 1
+
+    return distance
