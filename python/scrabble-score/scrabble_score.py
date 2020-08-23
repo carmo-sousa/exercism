d1ca023418e797847afbf6a75e739e952a9aa7ed
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-08-23 13:45:07 -0400

    Desafio scrabble score resolvido

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,23 @@
+
+dictionary = {'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1,
+              'N': 1, 'R': 1, 'S': 1, 'T': 1, 'D': 2, 'G': 2,
+              'B': 3, 'C': 3, 'M': 3, 'P': 3, 'F': 4, 'H': 4,
+              'V': 4, 'W': 4, 'Y': 4, 'K': 5, 'J': 8, 'X': 8,
+              'Q': 10, 'Z': 10}
+
+
+def score(word):
+    if len(word) == 0:
+        return 0
+
+    word = word.upper()
+    number_repetition_letter = {}
+    result = 0
+
+    for w in word:
+        number_repetition_letter[w] = word.count(w)
+
+    for w in number_repetition_letter:
+        result += dictionary[w] * number_repetition_letter[w]
+
+    return result
