9fd39fd957c0c4916447919dd16081d6bab69d0e
Carmo-sousa <carmosousa20@outlook.com>
2021-06-05 17:19:52 -0400

    Finally grade school

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,35 +1,19 @@
-"""
-{
-    1: [Romulo, Rodrigo, Rogério],
-    2: []
-}
-"""
-
-
 class School:
     def __init__(self):
-        self._students = []
-        self._grades = []
-        pass
+        self.students = {}
 
     def add_student(self, name, grade):
-        if grade in self._grades:
-            self._students[grade].append(name)
-
-        else:
-            self._students.insert(grade, [name])
+        self.students.setdefault(grade, []).append(name)
 
     def roster(self):
-        students = []
+        all_students = []
 
-        for item in self._students:
-            students.extend(item)
+        keys = sorted(self.students.keys())
 
-        return sorted(students)
+        for key in keys:
+            all_students.extend(sorted(self.students[key]))
+
+        return all_students
 
     def grade(self, grade_number):
-        if not self._grades and grade_number not in self._grades:
-            return []
-
-        else:
-            return self._students[grade_number]
+        return sorted(self.students.get(grade_number, []))
